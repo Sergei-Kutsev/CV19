b12c218ae4249c46d89278983f7b490370e29c0e
Сергей Куцев <sergeikutsev@yandex.ru>
2022-03-28 01:27:48 +0300

    Тестируем использование элементов WPF в библиотеках

---
+++ Tests/TestLib/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestLib
+{
+    public abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged //событие гененрируется, когда CanExecute меняет состояние из одного в другое
+        {
+            add => CommandManager.RequerySuggested += value; //передаем управление событием
+            remove => CommandManager.RequerySuggested += value;
+        }
+        public abstract bool CanExecute(object parameter); //функция которая возвращает истину/ложь. Если ложь, то команду выполнить нельзя,
+                                                           //если команду выполнить нельзя, то элемент к которому привязана команда отключчается
+                                                           //автоматически, т.е. когда мы описываем команды мы можем контролировать с их помощью
+                                                           //активность визуальных элементов на экране
+        public abstract void Execute(object parameter); // метод который будет выполен самой командой
+    }
+}
