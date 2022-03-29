f811f6f7a8ec4320962a22bb4e19b51a31cf2051
Сергей Куцев <sergeikutsev@yandex.ru>
2022-03-30 01:03:31 +0300

    Добавление в модель-представления Главного окна команду выхода из приложения

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,10 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
@@ -24,5 +27,29 @@ namespace CV19.ViewModels
             set => Set(ref _Title, value);
         }
         #endregion
+
+        #region Команды
+        //команда которая закрывает нашу программу
+        #region CloseApplicationCommand 
+        public ICommand CloseApplicationCommand { get;  } //Свойство - команда, которое закрывает программу
+
+        private bool CanCloseApplicationCommandExecute(object p) => true; //метод выполняется вместе с командой
+                    
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        public MainWindowViewModel() //внутри конструктора создаем значение команды
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+        }
     }
 }
