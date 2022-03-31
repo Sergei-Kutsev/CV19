d94dadef88bf9b2235c6b2802417b43a2ba95ccb
Сергей Куцев <sergeikutsev@yandex.ru>
2022-04-01 00:17:30 +0300

    Запрос к удаленному серверу для получения сырых данных

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace CV19Console
 {
     class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //находим данные в формате raw, копируем ссылку на них
+            //WebClient client = new WebClient(); //класс WebClient отправляет http запросы. С его помощью можно скачивать данные с любых сайтов
+
+            var client = new HttpClient(); //но в современнлм .NET core есть более новая и современная версия HttpClient
+
+            var response = client.GetAsync(data_url).Result; //получаем файл
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
