e207da25e28d7be839e8ecea0ef33a03659f0b58
Сергей Куцев <sergeikutsev@yandex.ru>
2022-04-14 21:35:07 +0300

    Дизайн главного окна для работы со списками студентов

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CV19.Infrastructure.Commands;
 using CV19.Models;
+using CV19.Models.Decanat;
 using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
@@ -11,6 +14,10 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        /*-------------------------------------------------------------------------------------------------------------------------*/
+
+        public ObservableCollection<Group> Groups { get; }
+
         #region SelectedPageIndex : int - Номер выбранной вкладки
         /// <summary>Номер выбранной вкладки</summary>
         private int _SelectedPageIndex;
@@ -24,7 +31,6 @@ namespace CV19.ViewModels
         }
 
         #endregion
-
 
         #region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор данных для визуализации графиков
         //скачиваем в Nuget пакет OxyPlot.WPF, с помощью которого можно рисовать графики
@@ -44,8 +50,10 @@ namespace CV19.ViewModels
 
         #endregion
 
+        #region Заголовок окна
+
         // в каждом поле должен быть вот такой вот код
-        #region "Заголовок окна"
+
         private string _Title = "Анализ статистики CV19";
 
         /// <summary>Заголовок окна</summary>
@@ -61,6 +69,8 @@ namespace CV19.ViewModels
             set => Set(ref _Title, value);
         }
         #endregion
+
+        /*-------------------------------------------------------------------------------------------------------------------------*/
 
         #region Команды
         //команда которая закрывает нашу программу
@@ -85,6 +95,8 @@ namespace CV19.ViewModels
 
         #endregion
 
+        /*-------------------------------------------------------------------------------------------------------------------------*/
+
         public MainWindowViewModel() //внутри конструктора создаем значение команды
         {
             #region Команды
@@ -103,6 +115,29 @@ namespace CV19.ViewModels
             }
 
             TestDataPoints = data_points;
+
+            var student_index = 1;
+            var students = Enumerable.Range(1, 10).Select(i => new Student
+            {
+                Name=$"Name {student_index}",
+                Surname = $"Surname {student_index}",
+                Patronymic = $"Patronymic {student_index++}",
+                Birthday = DateTime.Now,
+                Rating =0
+            });
+
+            var groups = Enumerable.Range(1, 20)//создадим перечисление целых чисел от 1 в количестве 20 штук
+                .Select(i => new Group//для каждого числа сделаем преобразование, преобразовывая число в группу
+            {
+                Name = $"Группа{i}",
+                Students = new ObservableCollection<Student>(students)
+            });
+
+            Groups = new ObservableCollection<Group>(groups);
+        
         }
+
+        /*-------------------------------------------------------------------------------------------------------------------------*/
+
     }
 }
